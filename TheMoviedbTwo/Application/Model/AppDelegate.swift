b6adf09db46932c50bf424af4a6efe47db67bd11
Aleksandr Bolotov <bolotov@siberian.pro>
2023-03-01 15:46:33 +0300

    init project

---
+++ TheMoviedbTwo/Application/Model/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TheMoviedbTwo
+//
+//  Created by Aleksandr Bolotov on 01.03.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
